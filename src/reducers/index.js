06de8bf818ae3741499900e70e98ad1198aea785
luojinghui <939035243@qq.com>
2016-11-01 15:45:32 +0800

    alter src

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 
 import { routerReducer as routing } from 'react-router-redux'
 import { combineReducers } from 'redux'
+import { initData } from './init'
 
 export default combineReducers({
-  routing
+  routing,
+  initData
 })
--- src/reducers/init.js
+++ src/reducers/init.js
@@ -1,2 +1,11 @@
-
+export const initData = (state = {
+    "item": []
+}, action) => {
+    switch(action.type) {
+        case 'initEnd':
+            return action.data
+        default :
+            return state
+    }
+}
 
