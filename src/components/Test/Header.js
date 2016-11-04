8b4cb7e00b14c16345b0c6fcb337b9ad27256fb5
luojinghui <939035243@qq.com>
2016-11-04 18:33:48 +0800

    add mini header.js

--- src/components/Test/Header.js
+++ src/components/Test/Header.js
@@ -3,3 +3,29 @@
  * Date: 16/11/4
  * Time: 下午3:18
  */
+import React, {Component} from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+            <header>
+                <ul>
+                    <li>
+                        <a className="on" href="">index</a>
+                    </li>
+                    <li>
+                        <a href="">weike</a>
+                    </li>
+                    <li>
+                        <a href="">serve</a>
+                    </li>
+                </ul>
+            </header>
+        );
+    }
+}
+
+Header.propTypes = {};
+Header.defaultProps = {};
+
+export default Header;
