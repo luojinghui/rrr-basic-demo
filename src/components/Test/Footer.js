fd8efdb168d4721439796b226e8069c741630b79
luojinghui <939035243@qq.com>
2016-11-04 18:33:28 +0800

    add mini footer .js

--- src/components/Test/Footer.js
+++ src/components/Test/Footer.js
@@ -3,3 +3,23 @@
  * Date: 16/11/4
  * Time: 下午3:19
  */
+import React, {Component} from 'react';
+
+class Footer extends Component {
+    render() {
+        return (
+            <footer>
+                <ul>
+                    <li>关于我们</li>
+                    <li>下载app</li>
+                    <li>联系我们</li>
+                </ul>
+            </footer>
+        );
+    }
+}
+
+Footer.propTypes = {};
+Footer.defaultProps = {};
+
+export default Footer;
