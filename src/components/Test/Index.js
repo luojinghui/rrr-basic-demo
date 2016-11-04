a7aa8b6f68f6f892ccc5198d5f8bf35e0c8a5ba2
luojinghui <939035243@qq.com>
2016-11-04 18:42:18 +0800

    add index

--- src/components/Test/Index.js
+++ src/components/Test/Index.js
@@ -3,3 +3,21 @@
  * Date: 16/11/4
  * Time: 下午3:00
  */
+import React, {Component} from 'react';
+
+class Index extends Component {
+    render() {
+        return (
+            <div className="two">
+                <div className="nav1">
+                    <h1>test one</h1>
+                </div>
+            </div>
+        );
+    }
+}
+
+Index.propTypes = {};
+Index.defaultProps = {};
+
+export default Index;
