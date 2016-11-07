e77101e46abb1aa3d2e88e5ec29556aba384f8e5
luojinghui <939035243@qq.com>
2016-11-07 15:50:27 +0800

    modifying the nav2 component's data is asyn

--- src/components/Test/NavTwo.js
+++ src/components/Test/NavTwo.js
@@ -3,13 +3,24 @@
  * Date: 16/11/4
  * Time: 下午6:50
  */
-import React, {Component} from 'react';
+import React, {Component} from 'react'
+import { connect } from 'react-redux'
 
 class NavTwo extends Component {
     render() {
+        const { nav2 } = this.props;
+
         return (
-            <div className="nav3">
-                <h1>test three</h1>
+            <div className="nav2">
+                {
+                    nav2.song ? (
+                        <div>
+                            <h3>{nav2.song}</h3>
+                            <h3>{nav2.author}</h3>
+                            <h3>{nav2.time}</h3>
+                        </div>
+                    ) : "loading..."
+                }
             </div>
         );
     }
@@ -18,4 +29,10 @@ class NavTwo extends Component {
 NavTwo.propTypes = {};
 NavTwo.defaultProps = {};
 
-export default NavTwo;
+const mapStateToProps = (state) => {
+    return {
+        nav2: state.initNav.nav2
+    }
+}
+
+export default connect(mapStateToProps)(NavTwo);
