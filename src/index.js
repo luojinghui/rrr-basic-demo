3f4efd408dad48ab384be50bc813c5b46f750cc1
luojinghui <939035243@qq.com>
2016-11-08 15:50:56 +0800

    modify index and delete no use code

--- src/index.js
+++ src/index.js
@@ -3,3 +3,45 @@
  * Date: 16/11/7
  * Time: 下午7:13
  */
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Router, Route, hashHistory, IndexRoute } from 'react-router'
+import { syncHistoryWithStore } from 'react-router-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux'
+import reducers from './reducers'
+import { initNavData, initData } from './actions'
+
+import Index from './components/Test/Index'
+import App from './components/Test/App'
+import NavOne from './components/Test/NavOne'
+import NavTwo from './components/Test/NavTwo'
+
+require('./base.css')
+
+const store = createStore(
+    reducers,
+    // applyMiddleware(thunk)
+    compose(
+        applyMiddleware(thunk),
+        window.devToolsExtension ? window.devToolsExtension() : f => f
+    )
+);
+const history = syncHistoryWithStore(hashHistory, store)
+
+store.dispatch(initNavData);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Router history={history}>
+            <Route path="/" component={App} >
+                <IndexRoute component={Index}/>
+                <Route path="nav1" component={NavOne}/>
+                <Route path="nav2" component={NavTwo}/>
+            </Route>
+        </Router>
+    </Provider>,
+    document.getElementById('root')
+)
